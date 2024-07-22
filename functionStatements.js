b6a65096b417e714ff4bb7f38cf073703d1ad815
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2024-07-22 18:27:25 +0530

    Delete functionStatements.js

--- functionStatements.js
+++
@@ -1,9 +0,0 @@
-//Function Statement (Declaration): Functions declared using the function keyword are called function statements or function declarations. They are hoisted to the top of their scope.
-
-function subtract(a,b){
-    return a-b;
-}
-console.log(subtract(10,4));
-
-//Hoisting: Function declarations are hoisted, meaning they can be called before their declaration in the code.
-
