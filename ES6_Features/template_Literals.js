d1d0e7a685cff17824e0b9929675402a114c8e74
Vaibhav <sukalevaibhav53@gmail.com>
2024-07-22 12:53:48 +0530

    added templateLiterals file

---
+++ ES6_Features/template_Literals.js
@@ -0,0 +1,13 @@
+//Template Literals allow embedding expressions and multi-line strings using backticks (```)
+
+const name = "John";
+const greeting = `Hello, ${name}! How are you ?`
+console.log(greeting)
+
+const multiLineString = `
+This is a string
+that spans multiple
+lines using template literals.
+`;
+
+console.log(multiLineString);
