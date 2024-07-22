6d52f94b658d45f90b09e3fd57ca51b4059bbb06
Vaibhav <sukalevaibhav53@gmail.com>
2024-07-22 18:26:23 +0530

    Added function

---
+++ FunctionExpressions.js
@@ -0,0 +1,16 @@
+//Function Expression: A function can be defined and assigned to a variable. This is known as a function expression.
+
+// Anonymous Function Expression: As shown above, the function does not have a name.
+const add = function(a,b){
+  return a+b;
+}
+
+console.log(add(5,3));
+
+// Named Function Expression: You can also provide a name to the function, which is useful for debugging.
+
+const multiply = function multiplyNumbers(a,b){
+    return a * b;
+};
+
+console.log(multiply(4,5));
