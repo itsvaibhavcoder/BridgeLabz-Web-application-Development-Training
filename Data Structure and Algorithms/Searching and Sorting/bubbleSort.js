3c24d9bedd8411fcdc6028b746f685b9e7c2481d
Vaibhav <sukalevaibhav53@gmail.com>
2024-08-09 11:21:28 +0530

    [smita]Added: Bubble Sort

---
+++ Data Structure and Algorithms/Searching and Sorting/bubbleSort.js
@@ -0,0 +1,16 @@
+function bblSort(arr) {
+    let n = arr.length;
+    for (var i = 0; i <n; i++) { 
+        for (var j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+            }
+        }
+    }
+    return arr;
+}
+
+var arr = [234, 43, 55, 63, 5, 6, 235, 547];
+console.log(bblSort(arr));
+
+
