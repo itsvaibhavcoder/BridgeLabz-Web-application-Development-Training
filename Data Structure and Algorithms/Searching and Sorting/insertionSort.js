42ee047a9a3900b711d283da95a758a9dc9dbdf6
Vaibhav <sukalevaibhav53@gmail.com>
2024-08-09 11:22:18 +0530

    [smita]Added: Insertion Sort

---
+++ Data Structure and Algorithms/Searching and Sorting/insertionSort.js
@@ -0,0 +1,20 @@
+function insertionSort(arr){
+    let n = arr.length;
+    for(let i=1; i<n; i++){
+        let temp = arr[i];
+        let j = i-1;
+        for(; j>=0; j--){
+            if(arr[j]>temp){
+                arr[j+1] = arr[j];
+            }
+            else{
+                break
+            }
+        }
+        arr[j+1] = temp;
+    }
+    return arr;
+}
+
+let arr = [5, 3, 8, 4, 2];
+console.log(insertionSort(arr));
