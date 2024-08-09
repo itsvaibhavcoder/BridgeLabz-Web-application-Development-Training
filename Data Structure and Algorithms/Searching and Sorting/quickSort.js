4acc6aa4e458224e3ca4423dc7a76d5fd61d1c7a
Vaibhav <sukalevaibhav53@gmail.com>
2024-08-09 11:24:25 +0530

    [smita]Added: Quick Sort

---
+++ Data Structure and Algorithms/Searching and Sorting/quickSort.js
@@ -0,0 +1,32 @@
+function partition(arr, low, high) {
+    let pivot = arr[low];
+    let i = low;
+    let j = high;
+
+    while (i < j) {
+        while (arr[i] <= pivot && i <= high - 1) {
+            i++;
+        }
+        while (arr[j] > pivot && j >= low + 1) {
+            j--;
+        }
+        if (i < j) {
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+    }
+    [arr[low], arr[j]] = [arr[j], arr[low]];
+    return j;
+}
+
+function quickSort(arr, low, high) {
+    if (low < high) {
+        let pIndex = partition(arr, low, high);
+        quickSort(arr, low, pIndex - 1);
+        quickSort(arr, pIndex + 1, high);
+    }
+}
+
+let arr = [4, 6, 2, 5, 7, 9, 1, 3];
+quickSort(arr, 0, arr.length - 1);
+
+console.log(arr);
