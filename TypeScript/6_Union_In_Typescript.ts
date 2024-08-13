90fa43749e5be61f92a402781deb37ab9949a878
Vaibhav <vaibhavsukale345@gmail.com>
2024-08-13 14:22:06 +0530

    [smita]added:Union in typeScript

---
+++ TypeScript/6_Union_In_Typescript.ts
@@ -0,0 +1,39 @@
+let score: number | string = 33
+score = 44
+score = "55"
+
+type user = {
+    name: string;
+    id: number
+}
+
+type admin = {
+    username: string;
+    id:number
+}
+
+let vaibhav: user | admin = {name: "vaibhav", id: 45}
+vaibhav = {username: "vs", id:334}
+
+// function getDbId(id: number | string){
+//     console.log(`DB id id: ${id}`);
+// }
+// getDbId(5);
+// getDbId("5")
+
+function getDbId(id: number | string){
+    if(typeof id==="string"){
+        return id.toLowerCase();
+    }
+}
+
+//array
+const data: number[]= [1,2,3]
+const data2: string[]= ["1","2","3"]
+const data3: string[] | number[] = [] //--It will store the all string type or number type
+
+const data4: (string | number| boolean)[] = ["1","2","3",4]
+
+let seatAllotment: "aisle"|"middle"|"window"
+seatAllotment = "aisle"
+// seatAllotment = "crew"
